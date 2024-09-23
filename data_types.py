ed89a70498c0e3d957bace84d7e1bab8c53387df
Melike Avsan <104467626+melikeavsan@users.noreply.github.com>
2024-09-23 15:26:28 +0300

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,92 @@
+#-------------------------------------------DATA TYPES----------------------------------------------
+
+x = "Hello World"
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = 44
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = 52.6
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = 2m
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = ["strawberry", "watermelon", "cherry"]
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = ("apricot", "grape", "pineapple")
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = range(3)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = {"name" : "Melike", "age" : 22}
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = {"peach", "banana", "orange"}
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = frozenset({"melon", "apple", "plum"})
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = True
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = b"Hello"
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = bytearray(9)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = memoryview(bytes(8))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = None
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
